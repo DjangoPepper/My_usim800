e380cf40d21f63381c6ebde464faa9e99f131481
DjangoPepper <fr3d.mobile@gmail.com>
2023-06-06 10:27:58 +0200

    voila

--- intro.py
+++ intro.py
@@ -7,12 +7,12 @@ def init(self):
 		self._vtd_stats = None
 		self._recog_stats = None
 		self._cmee_stats = None
-		_getsim_stats = _getsim_stats
+		self._getsim_stats = None
 		self._setsim_stats = None
 
 class Class_introdution:    
     #print (gsm.info.checkSim())
-    print (gsm.tools.get_simpin())
+    _getsim_stats = gsm.tools.get_simpin()
     # gsm.tools.set_simpin("0000")
     # gsm.tools.send_dtmf_code("5,1,0,2")
-    print(gsm.sql.get_appelant(1))
+    gsm.sql.get_appelant(1)
